dbe18138a32b60f4007f0d755aaa4e9636b46431
DELL <DELL@DESKTOP-G03MC99.mshome.net>
2023-02-12 13:46:46 +0530

    datat type added

---
+++ src/main/java/com/basic/DataType.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class DataType {
+
+	static int i;
+	static int i1=10;
+	
+	public static void main(String[] args) {
+		System.out.println(i);
+		System.out.println(i1);
+
+	}
+
+}
