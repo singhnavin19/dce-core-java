54481681f3f9787991ab50092279222f19bd1265
Navin Singh <singhnavin19290@gmail.com>
2023-02-14 21:59:38 +0530

    relational operator

---
+++ src/main/java/com/basic/RelationalOperator.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		int a=40,b=50;
+		
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+	}
+
+}
