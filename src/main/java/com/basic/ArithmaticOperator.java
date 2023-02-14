9464754c09339d2d0147ddad2168028b05b7edb2
Navin Singh <singhnavin19290@gmail.com>
2023-02-14 21:17:20 +0530

    operator

---
+++ src/main/java/com/basic/ArithmaticOperator.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class ArithmaticOperator {
+
+	public static void main(String[] args) {
+		int a=11,b=2;
+		
+		//Arithmatic Operation
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+
+}
