3dda9b516f1bfc149ad9097a78d7662cb74e25e0
Navin Singh <singhnavin19290@gmail.com>
2023-02-14 21:36:13 +0530

    assignment operator

--- src/main/java/com/basic/ArithmaticOperator.java
+++ src/main/java/com/basic/ArithmaticOperator.java
@@ -5,12 +5,14 @@ public class ArithmaticOperator {
 	public static void main(String[] args) {
 		int a=11,b=2;
 		
-		//Arithmatic Operation
+		//1. Arithmatic Operation
 		System.out.println(a+b);
 		System.out.println(a-b);
 		System.out.println(a*b);
 		System.out.println(a/b);
 		System.out.println(a%b);
+		
+		
 	}
 
 }
---
+++ src/main/java/com/basic/AssignmentOperator.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		int a=10,b=30;
+		System.out.println(a+=b); //a=a+b
+		System.out.println(a-=b); //a=a+b
+		System.out.println(a*=b); //a=a+b
+		System.out.println(a/=b); //a=a+b
+		System.out.println(a%=b); //a=a+b
+
+	}
+
+}
