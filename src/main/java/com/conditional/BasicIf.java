af4df82828e55a2595841583e3bcd2ce54303bde
Navin Singh <singhnavin19290@gmail.com>
2023-03-02 21:53:07 +0530

    if example

---
+++ src/main/java/com/conditional/BasicIf.java
@@ -0,0 +1,14 @@
+package com.conditional;
+
+public class BasicIf {
+
+	public static void main(String[] args) {
+		int age=2;
+		if(age>=18) {
+			System.out.println("eligible for vote");
+		}else {
+			System.out.println("Not eligible for vote");
+		}
+	}
+
+}
