82ddfcf0fb4590267467d827e5d04f2e5d81ca67
navinsingh <navinsingh@localhost>
2023-05-13 22:01:16 +0530

    constructor

--- src/main/java/com/oops/StudentWithConstructor.java
+++ src/main/java/com/oops/StudentWithConstructor.java
@@ -7,16 +7,35 @@ public class StudentWithConstructor {
 	void playing() {
 		System.out.println("playing");
 	}
+
+	public int giveMoney() {
+		System.out.println("give money");
+		return 10;
+	}
 	
 	 StudentWithConstructor() { //constructor 
-		System.out.println("StudentWithConstructor");
+		System.out.println("StudentWithConstructor 1");
+	}
+	 StudentWithConstructor(int i,double d) { //constructor 
+		 rollNo=100;
+		 s="navin";
+		System.out.println("StudentWithConstructor 2");
 	}
 	
 	public static void main(String[] args) {
-		StudentWithConstructor nikhil=new StudentWithConstructor();
-		System.out.println(nikhil.s);
-		nikhil.s="khelo";
-		System.out.println(nikhil.s);
+		StudentWithConstructor suraj=new StudentWithConstructor(10,2.34);
+		
+		int m=suraj.giveMoney();
+		suraj.playing();
+		System.out.println("roll nO is:-"+suraj.rollNo);
+		
+		StudentWithConstructor n2=new StudentWithConstructor();
+		
+		System.out.println(m);
+		
+		
+		
+		
 	}
 
 }
