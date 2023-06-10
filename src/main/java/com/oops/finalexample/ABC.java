dcc0590e85fe3c66fd9ae998a606a493802065ac
singhnavin19 <singhnavin19290@gmail.com>
2023-06-10 17:24:16 +0530

    remove compile error

---
+++ src/main/java/com/oops/finalexample/ABC.java
@@ -0,0 +1,17 @@
+package com.oops.finalexample;
+
+ class ABC {
+	
+	public static void main(String[] args) {
+		System.out.println("abc");
+	}
+}
+
+ class DEF{
+	public static void main(String[] args) {
+		System.out.println("def");
+	}
+}
+// public class XYZ{
+//	 
+// }
---
+++ src/main/java/com/oops/finalexample/AisFinal.java
@@ -0,0 +1,15 @@
+package com.oops.finalexample;
+
+final class AParent{
+	
+	int p=10;
+}
+
+public class AisFinal //extends AParent
+{
+	public static void main(String[] args) {
+		AisFinal a=new AisFinal();
+		
+//		System.out.println(a.p);
+	}
+}
