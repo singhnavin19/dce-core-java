84ada9c456fbdb16d22f8ac4fa6af4b54b8081f7
singhnavin19 <singhnavin19290@gmail.com>
2023-06-25 14:03:13 +0530

    arrayExample

---
+++ src/main/java/com/oops/array/ArrayBasicExample.java
@@ -0,0 +1,33 @@
+package com.oops.array;
+
+import java.util.Scanner;
+
+public class ArrayBasicExample {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		//dataType []varName=new dataType[]
+		  int         c[]  =new int[3];
+		  Scanner sc=new Scanner(System.in);
+		  for(int le=0;le<c.length;le++) {
+		      System.out.println("please enter elements");
+		  	  c[le]=sc.nextInt();
+		  }
+		  
+//		  c[2]='a';
+//		  c[0]='A';
+//		  c[1]='B';
+		  int z;//variable declaration
+		  //1. initialization 2. condtion check 3.increment/decrement
+		  //.4. body
+		  
+		  for(z=0;z<c.length;z++) { //z=1
+			  System.out.println(c[z]);
+		  }
+		  //0 1 2
+		  //System.out.println(c[z]); //java.lang.ArrayIndexOutOfBoundsException
+		  
+//		  int i[]=new int[2];
+//		  System.out.println(i[1]);
+	}
+
+}
--- src/main/java/com/oops/encapsulationexample/StudentMainClass.java
+++ src/main/java/com/oops/encapsulationexample/StudentMainClass.java
@@ -1,10 +1,17 @@
 package com.oops.encapsulationexample;
 
+import java.util.Date;
+
 public class StudentMainClass {
 	public static void main(String[] args) {
-		Student s=new Student(10,"navin","kandivali");
-		System.out.println(s.getName());
-		s.setAddress("Mira road");
+//		Student s=new Student(10,"navin","kandivali");
+//		System.out.println(s.getName());
+//		s.setAddress("Mira road");
+//	
+		Date d=new Date();
+		System.out.println(d);
+		String s="navin";
+		//java.lang
 		
 	}
 }
