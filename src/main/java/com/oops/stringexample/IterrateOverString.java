0c7238a3ba6ac54861155b947e0d784501e0c9be
singhnavin19 <singhnavin19290@gmail.com>
2023-07-16 14:12:41 +0530

    iterrate string

---
+++ src/main/java/com/oops/stringexample/IterrateOverString.java
@@ -0,0 +1,53 @@
+package com.oops.stringexample;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class IterrateOverString {
+	
+	public static void printStringUsingLoop(String str) {
+		System.out.println("***Using traditional loop**");
+		for(int i=0;i<str.length();i++) {
+			System.out.println(str.charAt(i));
+		}
+	}
+	
+	public static void printUsingEnchanceLoop(String s) {
+		System.out.println("***Using enchance loop**");
+		for(char c:s.toCharArray()) {
+			System.out.println(c);
+		}
+	}
+	
+	public static void printStringUsingSplit(String inp) {
+		String []output=inp.split("");
+		
+		for(String s:output) {
+			System.out.println(s);
+		}
+
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("please enter a string");
+		String input=sc.nextLine();
+		
+		printStringUsingLoop(input);
+	
+		printUsingEnchanceLoop(input);
+		
+		printStringUsingSplit(input);
+		
+		printStringUsingToken(input);
+	}
+
+	private static void printStringUsingToken(String input) {
+
+		StringTokenizer str=new StringTokenizer(input,"");
+		
+		while(str.hasMoreTokens()) {
+			System.out.println(str.nextToken());
+		}
+	}
+	
+}
