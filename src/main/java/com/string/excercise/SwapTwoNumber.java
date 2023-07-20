2157d0ee56bf7d63165cf0d8e76dcf79d6e632e7
singhnavin19 <singhnavin19290@gmail.com>
2023-07-20 21:01:23 +0530

    swap two numbers

---
+++ src/main/java/com/string/excercise/SwapTwoNumber.java
@@ -0,0 +1,32 @@
+package com.string.excercise;
+
+public class SwapTwoNumber {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		swapByUsingThirdVariable(a, b);
+		
+		SwapWithoutThirdVariable(a,b);
+	}
+
+	private static void SwapWithoutThirdVariable(int a, int b) {
+		System.out.println("old value of a="+a+"and b="+b);
+		
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		
+		System.out.println("new value of a="+a+"and b="+b);
+		
+	}
+
+	private static void swapByUsingThirdVariable(int a, int b) {
+		System.out.println("old value of a="+a+"and b="+b);
+		
+		int c=a;
+		a=b;
+		b=c;
+		
+		System.out.println("new value of a="+a+"and b="+b);
+	}
+}
