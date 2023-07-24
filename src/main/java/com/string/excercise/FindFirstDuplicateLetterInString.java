b3191c57ed783ab5279c826c4818cf51b63df9e8
singhnavin19 <singhnavin19290@gmail.com>
2023-07-24 22:06:56 +0530

    find duplicate word

--- src/main/java/com/string/excercise/FindFirstDuplicateLetterInString.java
+++ src/main/java/com/string/excercise/FindFirstDuplicateLetterInString.java
@@ -2,7 +2,20 @@ package com.string.excercise;
 
 public class FindFirstDuplicateLetterInString {
 	public static void main(String[] args) {
-		String name = "avinvi";
+		String name = "Hello Student Hello Sir";
+		
+		name.contains("sir");
+		
+		String arrayStrings[]=name.split(" ");
+		
+		for(int i=0;i<arrayStrings.length;i++) {
+			for(int j=i+1;j<arrayStrings.length;j++) {
+				if(arrayStrings[i].equals(arrayStrings[j])) {
+					System.out.println("bingo ,got element "+arrayStrings[i]);
+				}
+			}
+		}
+		
 		for (int j = 0; j < name.length(); j++) {// j=1
 			char c = name.charAt(j); // c=v
 			for (int i = j+1; i < name.length(); i++) {// i=2
