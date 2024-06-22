be832412bf24529b188b7a7b6a23c70ffec9fb54
singhnavin19 <singhnavin19290@gmail.com>
2024-06-22 21:53:13 +0530

    data type

---
+++ com/basic/variable/DefaultValues.java
@@ -0,0 +1,21 @@
+package com.basic.variable;
+
+public class DefaultValues {
+    static byte b;
+    static short s;
+    static int i;
+    static long l;
+    static float f;
+    static double d;
+    public static void main(String[] args) {
+
+        System.out.println("default value of byte is =" + b);
+        System.out.println("default value of short is =" + s);
+        System.out.println("default value of int is =" + i);
+        System.out.println("default value of long is =" + l);
+        System.out.println("default value of float is =" + f);
+        System.out.println("default value of double is =" + d);
+
+    }
+
+}
---
+++ com/basic/variable/VariableWithValue.java
@@ -0,0 +1,12 @@
+package com.basic.variable;
+
+public class VariableWithValue {
+    static float f=10.34f;
+    static byte b=(byte)-129;
+    static double d1=23d;
+    public static void main(String[] args) {
+        System.out.println(f);
+        System.out.println(b);
+        System.out.println(d1);
+    }
+}
