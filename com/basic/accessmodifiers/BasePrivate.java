2febc09e942a8bf991b950d9df05be4628c966a7
singhnavin19 <singhnavin19290@gmail.com>
2024-07-28 14:02:51 +0530

    accesmodifers

---
+++ com/basic/accessmodifiers/BasePrivate.java
@@ -0,0 +1,22 @@
+package com.basic.accessmodifiers;
+
+public class BasePrivate{
+    //acess data var = 10
+    private int privateI=10; //instance  //nhi
+    int defaultI=20; // nhi
+    protected int protectedI=30; // nhi
+    public int publicI=40;
+    public int getI(){
+        return privateI;
+    }
+
+    protected void printEvenOddNumberprotected(){
+        for (int i=0;i<=10;i++){
+            if(i%2==0){
+                System.out.println("Eeven number "+i);
+            }else{
+                System.out.println("odd Number "+i);
+            }
+        }
+    }
+}
