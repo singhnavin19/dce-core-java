b460ce3427c891afe01e05b85b0ab578151e57c4
singhnavin19 <singhnavin19290@gmail.com>
2024-08-24 14:24:18 +0530

    Update AnimalMainClass.java

--- com/basic/interfacceexample/AnimalMainClass.java
+++ com/basic/interfacceexample/AnimalMainClass.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class AnimalMainClass {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("animal name for animal sound (\n1. Dog, \n 2.Cat ,\n3 .KingKongMonkey)");
-        String input=sc.nextLine();
-        System.out.println("Animal name is "+input);
+        String input = sc.nextLine();
+        System.out.println("Animal name is " + input);
 
-        Animal animal=null;
-        if(input.equalsIgnoreCase("dog")){
-            animal=new Dog();
+        Animal animal = null;
+        if (input.equalsIgnoreCase("dog")) {
+            animal = new Dog();
         } else if (input.equalsIgnoreCase("Cat")) {
-            animal=new Cat();
-        }else if(input.equalsIgnoreCase("KingKongMonkey")){
-            animal=new KingKongMonkey();
-        }else{
+            animal = new Cat();
+        } else if (input.equalsIgnoreCase("KingKongMonkey")) {
+            animal = new KingKongMonkey();
+        } else {
             System.out.println("entered name is not correct or not in list ");
         }
-       if(animal!=null){
-           animal.sound();
-       }
+        if (animal != null) {
+            animal.sound();
+        }
 
 
     }
