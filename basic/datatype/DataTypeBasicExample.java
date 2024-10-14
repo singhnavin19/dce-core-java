1db65f9ee6eaece1d1de2b92018a32fb1d07bea4
singhnavin19 <singhnavin19290@gmail.com>
2024-10-14 21:27:15 +0530

    basic

---
+++ basic/datatype/DataTypeBasicExample.java
@@ -0,0 +1,15 @@
+//package basic.datatype;
+//
+//public class DataTypeBasicExample {
+//    static int age;
+//    static boolean test;
+//    static char c='A';
+//    public static void main(String[] args) {
+//       //dataType varName;
+//        System.out.println(age);
+//        System.out.println(test);
+//        System.out.println(c);
+//        System.out.println();
+//        sysout
+//    }
+//}
---
+++ basic/operators/Arithmatic.java
@@ -0,0 +1,16 @@
+package basic.operators;
+
+public class Arithmatic {
+    public static void main(String[] args) {
+        int a=11;
+        int b=3;
+        int c=a+b;
+        System.out.println("a="+a+" b="+b);
+        System.out.println("Addition is ="+c);
+        System.out.println("Subtraction="+(a-b));
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}
