33f844162a8cc4d94aac6cece4556120bcbea67f
singhnavin19 <singhnavin19290@gmail.com>
2025-05-31 09:34:18 +0530

    lecture 5

---
+++ basic/operators/UnaryBasicExample.java
@@ -0,0 +1,13 @@
+package basic.operators;
+
+public class UnaryBasicExample {
+    public static void main(String[] args) {
+        System.out.println("Unary Operator");
+        int a=10;
+        System.out.println("before "+a);
+        a++;
+        System.out.println("after1 ="+a);
+        ++a;
+        System.out.println("after2 ="+a);
+    }
+}
