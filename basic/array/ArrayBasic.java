b4eaacfda5db1f5a22c4ffae18c782bcd34dbe44
singhnavin19 <singhnavin19290@gmail.com>
2025-07-12 20:59:19 +0530

    oops1

--- basic/array/ArrayBasic.java
+++ basic/array/ArrayBasic.java
@@ -4,10 +4,10 @@ public class ArrayBasic {
     public static void main(String[] args) {
         int x=10;
         int x1[]={10,20,30,40}; //
-
-        for(int roomNo=0;roomNo<x1.length;roomNo++){ //roomNo=2
-            System.out.println(x1[roomNo]); //0-10 1-20 2-30 3-40 4
-        }
+//
+//        for(int roomNo=0;roomNo<x1.length;roomNo++){ //roomNo=2
+//            System.out.println(x1[roomNo]); //0-10 1-20 2-30 3-40 4
+//        }
 
         int x2[]=new int[10];
         x2[0]=78;
