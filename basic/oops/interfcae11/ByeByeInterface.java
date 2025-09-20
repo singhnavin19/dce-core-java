cfcebdc3d12d3c162bee9d6cec4ebb4e1e3cd416
singhnavin19 <singhnavin19290@gmail.com>
2025-09-20 15:41:37 +0530

    multiple interface

---
+++ basic/oops/interfcae11/ByeByeInterface.java
@@ -0,0 +1,7 @@
+package basic.oops.interfcae11;
+
+public interface ByeByeInterface {
+
+    public abstract void bybye();
+    public static final int j=20;
+}
--- basic/oops/interfcae11/MainClassInterface.java
+++ basic/oops/interfcae11/MainClassInterface.java
@@ -1,12 +1,26 @@
 package basic.oops.interfcae11;
 
+import java.util.Scanner;
+
 public class MainClassInterface {
     static void main(String[] args) {
-        HelloInterface helloInterface = new HelloInHindi();
-//         helloInterface.i=20;
-        System.out.println(helloInterface.i);
-        helloInterface.hello();
-        helloInterface = new HelloInEnglish();
-        helloInterface.hello();
+        System.out.println("please enter your choice \n 1, Hindi \n 2.English");
+        int language=new Scanner(System.in).nextInt();
+        HelloInterface helloInterface=null; ;
+
+        switch (language){
+            case 1:
+                   helloInterface=new HelloInHindi();
+                   break;
+            case 2:
+                helloInterface=new HelloInEnglish();
+                break;
+            default:
+                System.out.println("no hello bahar chalo");
+        }
+        if(helloInterface!=null){
+            helloInterface.hello();
+        }
+
     }
 }
