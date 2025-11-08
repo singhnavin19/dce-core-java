162b56a841cda1ade124dc8e3e48a7686c492cba
singhnavin19 <singhnavin19290@gmail.com>
2025-11-08 16:38:53 +0530

    String code
    string code

---
+++ basic/stringss/StringImmutable.java
@@ -0,0 +1,13 @@
+package basic.stringss;
+
+public class StringImmutable {
+    static void main(String[] args) {
+        String s="navin";
+        String s1="navin";
+        s=s.concat(" singh");
+
+        System.out.println(s);
+
+
+    }
+}
