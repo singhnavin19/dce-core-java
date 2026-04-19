869ada12a26c7e1b8f60a2527d7c8a7d0e9e4d6e
singhnavin19 <singhnavin19290@gmail.com>
2026-04-19 16:06:58 +0530

    hello world1

--- basic/hello/Hello.java
+++ basic/hello/Hello.java
@@ -2,6 +2,9 @@ package basic.hello;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");
+        int number=10;
+        byte b=11;
+        System.out.println(number);
+        System.out.println("Byte "+b);
     }
 }
